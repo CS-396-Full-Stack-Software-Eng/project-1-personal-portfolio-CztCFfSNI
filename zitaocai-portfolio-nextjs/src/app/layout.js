195e5d242c280d21af0bc0de4abfd8d49c83d7ac
cai.851 <cai.851@osu.edu>
2024-04-19 22:19:36 -0500

    dark mode done

--- zitaocai-portfolio-nextjs/src/app/layout.js
+++ zitaocai-portfolio-nextjs/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Providers from "./components/Provider";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <ThemeSwitcher />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
